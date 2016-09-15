5c2447240e2c1f3a26565c4b4f5456c501c35ded
nestoronald <ronald4261@hotmail.com>
2016-09-15 12:22:47 -0500

    Update settings.py
    quit

--- CongresoApp/settings.py
+++ CongresoApp/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db_congreso',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'wmaste',
-        'PASSWORD': 'igpwmaster',
+        'USER': 'wmaster',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
